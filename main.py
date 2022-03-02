8165fa2eaea3216ecafd0b61aaafb56784c243ae
coder-chekunkov@yandex.ru <coder-chekunkov@yandex.ru>
2022-03-02 21:08:38 +0300

    set up bot

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-print("Test commit: Alexandr")
-print("Test commit: Jakob")
+import telebot
+
+# Активирование токена и запуск бота:
+token = '5219565252:AAETCFyyTmY3ioY6yQr56Eiz5iTSdJ5jl4s'
+bot = telebot.TeleBot(token)
+
+
+# Тестовый метод:
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    bot.send_message(message.from_user.id, "what ")
+
+
+# Непрерывное прослушивание пользователя:
+bot.polling(none_stop=True, interval=0)
