695d20fd4da2c913f03ef3450e4230b6b877dc24
XmuRi1 <tsinkerman.yakov@yandex.ru>
2022-03-02 20:39:29 +0300

    set up tg_bot

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("Test commit: Alexandr")
-print("Test commit: Jakob")
+from telegram.ext import Updater, CommandHandler
+
+token = "5219565252:AAETCFyyTmY3ioY6yQr56Eiz5iTSdJ5jl4s"
+
+print("Бот запущен. Нажмите Ctrl+C для завершения")
+
+updater = Updater(token, use_context=True)
+
+updater.start_polling()
+updater.idle()
+
+
+
+
