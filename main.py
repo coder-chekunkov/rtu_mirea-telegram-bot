e07f8fd14c85a9a25efb2c6d15e7c58f6465f7c7
coder-chekunkov@yandex.ru <coder-chekunkov@yandex.ru>
2022-03-06 20:13:08 +0300

    add symptoms, preventions and link to developers

--- main.py
+++ main.py
@@ -25,13 +25,13 @@ def get_text_messages(message):
     elif message.text == "/stat":
         bot.send_message(message.from_user.id, "Статистика: в разработке!")
     elif message.text == "/symptoms":
-        bot.send_message(message.from_user.id, "Симптомы: в разработке!")
+        show_symptoms(message)
     elif message.text == "/prevention":
-        bot.send_message(message.from_user.id, "Профилактика: в разработке!")
+        show_prevention(message)
     elif message.text == "/questions":
         bot.send_message(message.from_user.id, "Вопросы: в разработке!")
     elif message.text == "/develop":
-        bot.send_message(message.from_user.id, "Разработчики: в разработке!")
+        show_develop(message)
     elif message.text == "/fact":
         bot.send_message(message.from_user.id, "Факты: в разработке!")
     else:
@@ -43,6 +43,21 @@ def show_bot_tasks(message):
     bot.send_message(message.from_user.id, text_of_messages.TEXT_TASKS, parse_mode="Markdown")
 
 
+# Метод отправки симптомов COVID-19:
+def show_symptoms(message):
+    bot.send_message(message.from_user.id, text_of_messages.TEXT_SYMPTOMS, parse_mode="Markdown")
+
+
+# Метод отправки профилактики COVID-19:
+def show_prevention(message):
+    bot.send_message(message.from_user.id, text_of_messages.TEXT_PREVENTION, parse_mode="Markdown")
+
+
+# Метод отправки контактов разработчиков:
+def show_develop(message):
+    bot.send_message(message.from_user.id, text_of_messages.TEXT_DEVELOP, parse_mode="Markdown",
+                     disable_web_page_preview=True)
+
+
 # Непрерывное прослушивание пользователя:
 bot.polling(none_stop=True, interval=0)
-bot.idle()
